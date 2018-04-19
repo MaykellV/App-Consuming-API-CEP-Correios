975e30334a9377d6e4e36e1a996046a91050d3f7
MaykellV <maykellvinicius@gmail.com>
2018-04-18 23:52:36 -0300

    Consuming api correios

---
+++ AppConsultaCep/AppConsultaCepPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AppConsultaCep.Model;
+using AppConsultaCep.Service;
+using Xamarin.Forms;
+
+namespace AppConsultaCep
+{
+    public partial class AppConsultaCepPage : ContentPage
+    {
+        public AppConsultaCepPage()
+        {
+            InitializeComponent();
+
+            BUTTON.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs e)
+        {
+            string cep = CEP.Text.Trim();
+            Address address = ViaCepService.SearchAddressToCep(cep);
+
+            RESULT.Text = String.Format("Endereço: {3}, {0}, {1}, {2}", 
+                                        address.Localidade, address.Uf, address.Logradouro, address.Bairro);
+        }
+    }
+}
---
+++ AppConsultaCep/Service/ViaCepService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using AppConsultaCep.Model;
+using Newtonsoft.Json;
+
+namespace AppConsultaCep.Service
+{
+    public class ViaCepService
+    {
+        private static string addressURL = "http://viacep.com.br/ws/{0}/json";
+
+        public static Address SearchAddressToCep(string cep)
+        {
+            addressURL = String.Format(addressURL, cep);
+            HttpClient httpClient = new HttpClient();
+
+            var response = httpClient.GetAsync(addressURL).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().Result ;
+                string addressResult = content;
+
+                Address address = JsonConvert.DeserializeObject<Address>(addressResult);
+
+                return address;
+            }
+
+            return null;
+        }
+    }
+}
